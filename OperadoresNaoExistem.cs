63caf5328f365417b600f3549eca0d7dd1743936
Giovanni Bassi <giggio@giggio.net>
2020-12-08 16:42:35 -0300

    Operadores não existem

---
+++ OperadoresNaoExistem.cs
@@ -0,0 +1,27 @@
+﻿namespace NonEcsiste.OperadoresNaoExistem
+{
+    public class OperadoresNaoExistem
+    {
+        private static readonly Person pessoa = new Person(null);
+
+        void M()
+        {
+            var nome = pessoa?.Nome;
+        }
+
+        void N()
+        {
+            var outraPessoa = pessoa ?? new Person(null);
+        }
+        void O()
+        {
+            Person maisOutraPessoa;
+            if (pessoa is null)
+                maisOutraPessoa = pessoa;
+            else
+                maisOutraPessoa = new Person(null);
+        }
+
+        record Person(string Nome);
+    }
+}
