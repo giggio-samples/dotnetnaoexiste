5f91c6125ccacb8bf7e7ed77c7f70d673ed9020c
Giovanni Bassi <giggio@giggio.net>
2023-04-14 23:05:39 -0300

    Add Primary Constructor example

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using NonEcsiste.Objetos;
+﻿using NonEcsiste;
+using NonEcsiste.LinqNaoExiste;
+using NonEcsiste.Objetos;
 
 LinqNaoExiste.LinqToGiggio();
 var gandhi = new Humano("Mahatma Gandhi");
@@ -6,3 +8,5 @@ WriteLine(gandhi.Nome());
 var chimpanze = new Chimpanze();
 WriteLine(chimpanze.Nome());
 ReadLine();
+var student = new PrimaryConstructor_Student(1, "Giovanni", new[] { 6.7m, 8.5m });
+WriteLine(student.GPA);
